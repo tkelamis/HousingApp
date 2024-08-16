df0fc11a830db0200b5dccda28f1a98d87282a99
tkelamis <kelamis.anastasios@gmail.com>
2024-08-16 17:59:31 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent, RouterModule],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyFirstApp';
+  title = 'homes';
 }
